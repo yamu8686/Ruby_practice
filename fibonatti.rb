37ec51078a667e5a685a7c842362a196208b7045
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-03-28 07:47:42 +0000

    fibonacci

--- fibonatti.rb
+++ fibonatti.rb
@@ -1,16 +1,7 @@
 def fibonacci(n)
-  array = [1,0,5]
-  return array[n] if n <= 1
-  fibonacci(n - 1) + fibonacci(n - 3)
+  return n if n <= 1
+  fibonacci(n - 1) + fibonacci(n - 2)
 end
 
-puts "何番目の数字をもとめますか？"
-t = gets.to_i
-n = 0
-while n < t
-  d = a+c
-  d = b+d
-  n += 1
-end
-
-puts "#{t}番目の数字は#{fibonacci(n)}です"
+n = 10
+puts fibonacci(n)
